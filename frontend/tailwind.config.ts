d5a2f309259636d1c01afa12e552319d1bf9ab20
RubenRoman <ruben.roman@forcast.cl>
2024-03-07 23:58:24 -0500

    :lipstick: primary color added to tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#fdec3c',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
